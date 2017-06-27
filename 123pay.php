f104d18b90ca984116a3cb482e7d5d4519389c14
Hojjat Ali Mohammadi <safeith@riseup.net>
2017-06-27 12:15:39 +0430

    Update Code Style

--- 123pay.php
+++ 123pay.php
@@ -6,21 +6,21 @@
 <?php
 require_once __DIR__ . '/includes/123pay.php';
 
-$merchant_id = $_POST['merchant_id'];
-$amount = ($_POST['currencies'] == 'Toman') ? ((int)($_POST['amount'])) * 10 : ((int)($_POST['amount']));
+$merchant_id  = $_POST['merchant_id'];
+$amount       = ( $_POST['currencies'] == 'Toman' ) ? ( (int) ( $_POST['amount'] ) ) * 10 : ( (int) ( $_POST['amount'] ) );
 $callback_url = $_POST['systemurl'] . 'modules/gateways/callback/123pay.php?invoiceid=' . $_POST['invoiceid'];
 
-if (!extension_loaded('curl')) {
-    echo 'خطا : curl فعال نیست';
-    exit();
+if ( ! extension_loaded( 'curl' ) ) {
+	echo 'خطا : curl فعال نیست';
+	exit();
 }
 
-$response = create($merchant_id, $amount, $callback_url);
-$result = json_decode($response);
-if ($result->status) {
-    header('Location:' . $result->payment_url);
+$response = create( $merchant_id, $amount, $callback_url );
+$result   = json_decode( $response );
+if ( $result->status ) {
+	header( 'Location:' . $result->payment_url );
 } else {
-    echo 'خطا : ' . $result->message;
+	echo 'خطا : ' . $result->message;
 }
 ?>
 </body>
--- modules/gateways/_123pay.php
+++ modules/gateways/_123pay.php
@@ -1,48 +1,46 @@
 <?php
-if (!defined("WHMCS")) {
-    die("This file cannot be accessed directly");
+if ( ! defined( "WHMCS" ) ) {
+	die( "This file cannot be accessed directly" );
 }
 
-function _123pay_MetaData()
-{
-    return array(
-        'DisplayName' => 'سامانه پرداخت اینترنتی یک دو سه پی',
-        'APIVersion' => '1.0',
-        'DisableLocalCredtCardInput' => false,
-        'TokenisedStorage' => false
-    );
+function _123pay_MetaData() {
+	return array(
+		'DisplayName'                => 'سامانه پرداخت اینترنتی یک دو سه پی',
+		'APIVersion'                 => '1.0',
+		'DisableLocalCredtCardInput' => false,
+		'TokenisedStorage'           => false
+	);
 }
 
-function _123pay_config()
-{
-    $configarray = array(
-        "FriendlyName" => array(
-            "Type" => "System",
-            "Value" => "سامانه پرداخت اینترنتی یک دو سه پی"
-        ),
-        "merchant_id" => array(
-            "FriendlyName" => "merchant_id",
-            "Type" => "text",
-            "size" => "36"
-        ),
-        "Currencies" => array(
-            "FriendlyName" => "Currencies",
-            "Type" => "dropdown",
-            "Options" => "Rial,Toman"
-        )
-    );
-    return $configarray;
+function _123pay_config() {
+	$configarray = array(
+		"FriendlyName" => array(
+			"Type"  => "System",
+			"Value" => "سامانه پرداخت اینترنتی یک دو سه پی"
+		),
+		"merchant_id"  => array(
+			"FriendlyName" => "merchant_id",
+			"Type"         => "text",
+			"size"         => "36"
+		),
+		"Currencies"   => array(
+			"FriendlyName" => "Currencies",
+			"Type"         => "dropdown",
+			"Options"      => "Rial,Toman"
+		)
+	);
+
+	return $configarray;
 }
 
-function _123pay_link($params)
-{
-    $merchant_id = trim($params['merchant_id']);
-    $amount = $params['amount'];
-    $systemurl = $params['systemurl'];
-    $currencies = $params['Currencies'];
-    $invoiceid = $params['invoiceid'];
-    $currencies = $params['Currencies'];
-    $code = '
+function _123pay_link( $params ) {
+	$merchant_id = trim( $params['merchant_id'] );
+	$amount      = $params['amount'];
+	$systemurl   = $params['systemurl'];
+	$currencies  = $params['Currencies'];
+	$invoiceid   = $params['invoiceid'];
+	$currencies  = $params['Currencies'];
+	$code        = '
 			<form action="./123pay.php" method="POST">
 				<input type="hidden" name="merchant_id" value="' . $merchant_id . '" />
 				<input type="hidden" name="amount" value="' . $amount . '" />
@@ -52,7 +50,8 @@ function _123pay_link($params)
 				<input type="submit" name="pay" value="  پرداخت آنلاین  " />
 			</form>
 			';
-    return $code;
+
+	return $code;
 }
 
 ?>
